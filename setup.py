eeb05851e44784fb3c349dd8837e8467146f1005
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-06-27 08:47:41 +0530

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
-from setuptools import setup, find_packages
 import io
 import os
+from setuptools import setup, find_packages
 
+# Function to read the contents of a file
+def read(*paths, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        return open_file.read().strip()
+
+# Read the version from the VERSION file
 def read_version():
     version_file = os.path.join(os.path.dirname(__file__), "src", "VERSION")
     with io.open(version_file, encoding="utf-8") as f:
         version = f.read().strip()
     return version
 
-def read_requirements(filename):
-    with io.open(filename, encoding="utf-8") as f:
-        return f.read().splitlines()
+# Function to read the requirements from a file
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
 
 setup(
     name="pythonassesment",
@@ -30,11 +43,11 @@ setup(
             'pythonassesment = pythonassesment.__main__:main',
         ],
     },
-    classifiers={
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    },
+    ],
     python_requires='>=3.6',
 )
 
